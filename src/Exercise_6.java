c3b00b07f9c616ff0babe4b3a685a7b077984f28
lemon <lemon.juice9341@gmail.com>
2023-08-09 18:01:56 +0300

    In this class the array is sorted

---
+++ src/Exercise_6.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+public class Exercise_6 {
+    public static void main(String[] args) {
+        String[] str={"Alex","Roma", "Dovlet","Vlad","Egor"};
+        Arrays.sort(str);
+        System.out.println(Arrays.toString(str));
+    }
+}
